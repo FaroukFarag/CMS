bcad4cc3b957a25c881d00f8caa159629e6be943
Farouk Farag <farouk.farag98@gmail.com>
2024-06-01 00:02:11 +0400

    Create UI Components

--- CMS.Client/src/app/app.routes.ts
+++ CMS.Client/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './customers/customers.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
+import { EditCustomerComponent } from './customers/edit-customer/edit-customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'customers',
+        children: [
+          { path: '', component: CustomersComponent },
+          { path: 'create', component: CreateCustomerComponent },
+          { path: 'edit/:id', component: EditCustomerComponent }
+        ]
+       },
+      { path: '',   redirectTo: '/customers', pathMatch: 'full' },
+      { path: '**', component: NotFoundComponent }
+];
